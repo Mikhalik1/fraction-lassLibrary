1947cd2b57c24ff32a2055f0d561caaaead2a688
rotoyouoi <rotoyouoi@yandex.ru>
2023-09-26 15:04:00 +0500

    Добавьте файлы проекта.

---
+++ Drob.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fractionСlassLibrary
+{
+    public class Drob
+    {
+        public string FixWrongResult(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new Exception("Пустота фвф");
+            }
+            if (text == "/")
+            {
+                throw new Exception("Пустота фвф");
+            }
+
+            
+            string[] number = text.Split('/');
+
+
+            if (number.Length >2)
+            {
+                throw new Exception("Пустота фвф");
+            }
+
+            if (Convert.ToDouble(number[0]) > Convert.ToDouble(number[1]) )
+            {
+                number[0] = number[1];
+            }
+
+            
+            string newtextstring = string.Join("/", number);
+            return newtextstring;
+        }
+    }
+}
---
+++ UnitTestProject1/factoriotest.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using fractionСlassLibrary;
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class factoriotest
+    {
+        [TestMethod]
+        public void AreNotEqual_Test()
+        {
+            //arc
+            string text = "1/1";
+            //arrange
+            Drob obj = new Drob();
+            string result = obj.FixWrongResult(text);
+            string expt = "10/10";
+            //assets
+            Assert.AreNotEqual(expt, result);
+
+        }
+
+        [TestMethod]
+        public void FractionTest_Correct_True()
+        {
+            //arc
+            string text = "1/10";
+            //arrange
+            Drob obj = new Drob();
+            string result = obj.FixWrongResult(text);
+            string expt = "1/10";
+            //assets
+            Assert.AreEqual(expt, result);
+        }
+        [TestMethod]
+        public void FractionTest_CorrectReplace_True()
+        {
+            //arc
+            string text = "256/10";
+            //arrange
+            Drob obj = new Drob();
+            string result = obj.FixWrongResult(text);
+            string expt = "10/10";
+            //assets
+            Assert.AreEqual(expt, result);
+        }
+        [TestMethod]
+        public void FractionTest_CorrectMinus_True()
+        {
+            //arc
+            string text = "-10/10";
+            //arrange
+            Drob obj = new Drob();
+            string result = obj.FixWrongResult(text);
+            string expt = "-10/10";
+            //assets
+            Assert.AreEqual(expt, result);
+        }
+        
+        
+        [TestMethod]
+        public void FractionTest_Empty_False()
+        {
+            //arc
+            string text = "";
+            //arrange
+            Drob obj = new Drob();
+            
+            Action actiona = () => obj.FixWrongResult(text);
+            
+            //assets
+            Assert.ThrowsException<Exception>(actiona);
+        }
+        [TestMethod]
+        public void FractionTest_EmptyDash_False()
+        {
+            //arc
+            string text = "/";
+            //arrange
+            Drob obj = new Drob();
+
+            Action actiona = () => obj.FixWrongResult(text);
+
+            //assets
+            Assert.ThrowsException<Exception>(actiona);
+        }
+        [TestMethod]
+        public void FractionTest_Manynumber_False()
+        {
+            //arc
+            string text = "35/10/10/25";
+            //arrange
+            Drob obj = new Drob();
+
+            Action actiona = () => obj.FixWrongResult(text);
+
+            //assets
+            Assert.ThrowsException<Exception>(actiona);
+        }
+
+        [TestMethod]
+        public void FractionTest_letters_False()
+        {
+            //arc
+            string text = "35/1b";
+            //arrange
+            Drob obj = new Drob();
+
+            Action actiona = () => obj.FixWrongResult(text);
+
+            //assets
+            Assert.ThrowsException<FormatException>(actiona);
+        }
+
+
+        [TestMethod]
+        public void FractionTest_onlynumbers_False()
+        {
+            //arc
+            string text = "3512";
+            //arrange
+            Drob obj = new Drob();
+
+            Action actiona = () => obj.FixWrongResult(text);
+
+            //assets
+            Assert.ThrowsException<IndexOutOfRangeException>(actiona);
+        }
+    }
+}
